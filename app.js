29c263b6bb28ea88f6d45992cb0e8a9c42ae510f
Wagner Pedro <wagnerpedro355@gmail.com>
2023-03-29 11:32:16 -0300

    logica inicial com javascript

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const btnAddtask = document.querySelector("span.btn-add")
+const containerTasks = document.querySelector(".tasks")
 
-btnAddtask.addEventListener("click", (e)=> {
-    
+// Evento ouvidor, chama uma caixa de notas
+btnAddtask.addEventListener("click", (e) => {
+    const modal = document.querySelector(".note-box")
+    modal.style.display = 'block'
+    //pega botões do modal
+    // top buttoms
+    const moreNoteInputBtn = document.querySelector(".note-box-btn")
+    const moreNoteOptions = document.querySelector(".more-options-btn")
+    const closeAndSave = document.querySelector(".close-note-box-btn")
+    // bottom buttoms
+    const boldBtn = document.querySelector(".bold-btn")
+    const italicBtn = document.querySelector(".italic-btn")
+    const underlineBtn = document.querySelector(".underline-btn")
+    const listBtn = document.querySelector(".list-btn")
+    // pega dados do textarea
+    const inputNote = document.getElementById("input-note")
 })
