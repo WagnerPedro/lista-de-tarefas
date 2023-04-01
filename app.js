d2c7880211becfa5c6dea99e76cc67bc29b2cbae
Wagner Pedro <wagnerpedro355@gmail.com>
2023-04-01 19:50:28 -0300

    logica nova desorganizada

--- app.js
+++ app.js
@@ -3,6 +3,21 @@
 let addTask = document.querySelector(".btn-add");
 let noteWindow = document.querySelector(".note-box");
 let moreTasks = document.querySelector(".note-box-btn");
-let moreOptions = document.querySelector(".more-options-btn");
+let moreOptionsBtn = document.querySelector(".more-options-btn");
+let colorsModal = document.querySelector(".more-options")
 let closeModal = document.querySelector(".close-note-box-btn");
 let modalContent = document.getElementById("input-note");
+
+function mostraModal(addTask) {
+  addTask.addEventListener("click", ()=> {
+    console.log('it worked!')
+  })
+}
+
+addTask.addEventListener("click", mostraModal(addTask))
+
+function showColorsOption(moreOptions) {
+  moreOptions.addEventListener("click", ()=>{
+    colorsModal.classList.add("showColors")
+  })
+}
