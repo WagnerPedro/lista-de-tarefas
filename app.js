9f0ac1b8176a65ad03af917107bbe85a4aed8442
Wagner Pedro <wagnerpedro355@gmail.com>
2023-04-04 20:30:58 -0300

    funcionanlidade de fechar modal

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 "use strict";
 
 let addTask = document.querySelector(".btn-add");
-let noteWindow = document.querySelector(".note-box");
+let modal = document.querySelector(".note-box");
 let moreTasks = document.querySelector(".note-box-btn");
 let moreOptionsBtn = document.querySelector(".more-options-btn");
-let colorsModal = document.querySelector(".more-options")
+let colorsModal = document.querySelector(".more-options");
 let closeModal = document.querySelector(".close-note-box-btn");
 let modalContent = document.getElementById("input-note");
 
-function mostraModal(addTask) {
-  addTask.addEventListener("click", ()=> {
-    noteWindow.style.display = 'block'
-  })
+// Abrir modal
+function mostrarModal(addTask) {
+  addTask.addEventListener("click", () => {
+    modal.style.display = "block";
+  });
 }
 
-addTask.addEventListener("click", mostraModal(addTask))
+addTask.addEventListener("click", mostrarModal(addTask));
 
+// Fechar modal
+function fecharModal(closeModal, modal) {
+  closeModal.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+}
+
+closeModal.addEventListener("click", fecharModal(closeModal, modal));
