fddca84d7132fff000c61c22445d8fa0da6373a8
Wagner Pedro <wagnerpedro355@gmail.com>
2023-04-08 14:32:34 -0300

    programando com modulos/import export

---
+++ js/app.js
@@ -0,0 +1,11 @@
+"use strict";
+
+import funcoes from "./funcionalidades.js";
+
+// let addTask = document.querySelector(".btn-add");
+// let modal = document.querySelector(".note-box");
+// let moreTasks = document.querySelector(".note-box-btn");
+// let moreOptionsBtn = document.querySelector(".more-options-btn");
+// let colorsModal = document.querySelector(".more-options");
+// let closeModal = document.querySelector(".close-note-box-btn");
+// let modalContent = document.getElementById("input-note");
